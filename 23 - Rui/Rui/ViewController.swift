2af6a07a03e9af0005f11d01738d25812ddfad21
Luiz Carlos <lucachaves@gmail.com>
2017-03-06 14:50:00 -0300

    Add 06/03/2017

---
+++ 23 - Rui/Rui/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rui
+//
+//  Created by admin on 06/03/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
